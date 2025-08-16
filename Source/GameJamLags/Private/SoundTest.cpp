ce1990c4738615558c0f795198075126ae1158a9
migueahumada <migangahuga@gmail.com>
2025-08-15 19:10:06 -0600

    Integrated C++ in the game

---
+++ Source/GameJamLags/Private/SoundTest.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SoundTest.h"
+#include "Components/AudioComponent.h"
+#include "Sound/SoundBase.h"
+
+// Sets default values
+ASoundTest::ASoundTest()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	m_pAudioComponent = CreateDefaultSubobject<UAudioComponent>(FName("Audio"));
+
+	m_pAudioComponent->SetupAttachment(RootComponent);
+	
+	m_pAudioComponent->SetSound(m_pSound);
+}
+
+// Called when the game starts or when spawned
+void ASoundTest::BeginPlay()
+{
+	Super::BeginPlay();
+	m_pAudioComponent->Play();
+	
+	
+}
+
+// Called every frame
+void ASoundTest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/GameJamLags/Public/SoundTest.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SoundTest.generated.h"
+
+class AudioComponent;
+class SoundBase;
+
+UCLASS()
+class GAMEJAMLAGS_API ASoundTest : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASoundTest();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UAudioComponent> m_pAudioComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<USoundBase> m_pSound;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
