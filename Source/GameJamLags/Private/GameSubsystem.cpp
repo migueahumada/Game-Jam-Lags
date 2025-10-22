96e9d8f878e10c34188a22c496321de76d428c4a
migueahumada <migangahuga@gmail.com>
2025-10-22 17:36:08 -0600

    Arrglando sistema de UI

--- Source/GameJamLags/Private/GameSubsystem.cpp
+++ Source/GameJamLags/Private/GameSubsystem.cpp
@@ -2,4 +2,85 @@
 
 
 #include "GameSubsystem.h"
+#include "Blueprint/UserWidget.h"
+#include "Kismet/GameplayStatics.h" 
 
+
+
+
+void UGameSubsystem::InitWidgets()
+{
+	
+	APlayerController* playerController = UGameplayStatics::GetPlayerController(GetWorld(),0);
+
+	if (!playerController)
+	{
+		return;
+	}
+
+	//Mostly used Widgets
+
+	m_pauseWidget = CreateWidget<UUserWidget>(playerController, m_pauseWidgetClass);
+
+	//LevelWidgets
+	for (auto& widgetClass : m_widgetClasses)
+	{
+		if (!widgetClass)
+		{
+			continue;
+		}
+
+		UUserWidget* widgetPtr = CreateWidget<UUserWidget>(playerController,widgetClass);
+
+		if (!widgetPtr)
+		{
+			continue;
+		}
+		widgetPtr->SetVisibility(ESlateVisibility::Hidden);
+		widgetPtr->AddToViewport();
+
+		m_activeWidgets.Add(widgetPtr);
+	}
+}
+
+void UGameSubsystem::ShowWidget(TSubclassOf<UUserWidget> widgetClass)
+{
+	APlayerController* playerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+
+	if (!playerController)
+	{
+		return;
+	}
+
+	for (auto& currentWidget : m_activeWidgets)
+	{
+		if (currentWidget && currentWidget->IsA(widgetClass))
+		{
+			//playerController->SetInputMode(FInputModeUIOnly());
+			currentWidget->SetVisibility(ESlateVisibility::Visible);
+			//playerController->bShowMouseCursor = true;
+			break;
+		}
+	}
+}
+
+void UGameSubsystem::HideWidget(TSubclassOf<UUserWidget> widgetClass)
+{
+	APlayerController* playerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+
+	if (!playerController)
+	{
+		return;
+	}
+
+	for (auto& currentWidget : m_activeWidgets)
+	{
+		if (currentWidget && currentWidget->IsA(widgetClass))
+		{
+			//playerController->SetInputMode(FInputModeGameOnly());
+			currentWidget->SetVisibility(ESlateVisibility::Hidden);
+			//playerController->bShowMouseCursor = false;
+			break;
+		}
+	}
+}
--- Source/GameJamLags/Public/GameSubsystem.h
+++ Source/GameJamLags/Public/GameSubsystem.h
@@ -6,6 +6,9 @@
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "GameSubsystem.generated.h"
 
+class UUserWidget;
+class UWorld;
+
 /**
  * 
  */
@@ -13,5 +16,38 @@ UCLASS(Blueprintable, Abstract)
 class GAMEJAMLAGS_API UGameSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
+
 	
+private:
+
+	
+	UPROPERTY(EditAnywhere, Category = "Widgets")
+	TArray<UUserWidget*> m_activeWidgets;
+
+	UPROPERTY(EditAnywhere, Category = "Widgets")
+	TArray<TSubclassOf<UUserWidget>> m_widgetClasses;
+
+	//Pause Widgets
+	UPROPERTY(VisibleAnywhere, Category = "Widgets")
+	TSubclassOf<UUserWidget> m_pauseWidgetClass;
+
+	UPROPERTY(VisibleAnywhere, Category = "Widgets")
+	UUserWidget* m_pauseWidget;
+
+	UPROPERTY(VisibleAnywhere, Category = "Widgets")
+	UUserWidget* m_menuWidget;
+
+public:
+	
+	
+
+	UFUNCTION(BlueprintCallable)
+	void InitWidgets();
+
+	UFUNCTION(BlueprintCallable)
+	void ShowWidget(TSubclassOf<UUserWidget> widgetClass);
+
+	UFUNCTION(BlueprintCallable)
+	void HideWidget(TSubclassOf<UUserWidget> widgetClass);
+
 };
